cfc76f06fa23f36c4a336a5a10fa1f9f6279f5c1
Skinay <github@skinay.ch>
2026-06-10 23:11:42 +0200

    Will this work ?

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '../../../amplify/data/resource';
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "../../../amplify/data/resource";
+import { Auth } from "aws-amplify";
+import { AuthStateService } from "@aws-amplify/ui-angular";
 
 const client = generateClient<Schema>();
 
 @Component({
-  selector: 'app-todos',
+  selector: "app-todos",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css',
+  templateUrl: "./todos.component.html",
+  styleUrl: "./todos.component.css",
 })
 export class TodosComponent implements OnInit {
   todos: any[] = [];
+  authState: string = "loading";
+  user: any;
+
+  constructor(private authStateService: AuthStateService) {
+    this.authStateService.authState$.subscribe((authState) => {
+      this.authState = authState;
+      if (authState === "authenticated") {
+        Auth.currentAuthenticatedUser().then((user) => (this.user = user));
+      }
+    });
+  }
+
+  async signOut() {
+    await Auth.signOut();
+  }
 
   ngOnInit(): void {
     this.listTodos();
@@ -27,18 +44,18 @@ export class TodosComponent implements OnInit {
         },
       });
     } catch (error) {
-      console.error('error fetching todos', error);
+      console.error("error fetching todos", error);
     }
   }
 
   createTodo() {
     try {
       client.models.Todo.create({
-        content: window.prompt('Todo content'),
+        content: window.prompt("Todo content"),
       });
       this.listTodos();
     } catch (error) {
-      console.error('error creating todos', error);
+      console.error("error creating todos", error);
     }
   }
 }
