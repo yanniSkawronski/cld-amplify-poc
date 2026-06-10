5a5435927726a95e98613e7fa4e89108f0a57076
Skinay <github@skinay.ch>
2026-06-10 23:39:08 +0200

    Delete auth definition

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "../../../amplify/data/resource";
-import { Auth } from "aws-amplify";
-import { AuthStateService } from "@aws-amplify/ui-angular";
 
 const client = generateClient<Schema>();
 
@@ -18,19 +16,6 @@ export class TodosComponent implements OnInit {
   todos: any[] = [];
   authState: string = "loading";
   user: any;
-
-  constructor(private authStateService: AuthStateService) {
-    this.authStateService.authState$.subscribe((authState) => {
-      this.authState = authState;
-      if (authState === "authenticated") {
-        Auth.currentAuthenticatedUser().then((user) => (this.user = user));
-      }
-    });
-  }
-
-  async signOut() {
-    await Auth.signOut();
-  }
 
   ngOnInit(): void {
     this.listTodos();
